e0f7743afe9ef21ede6bb960084c4bc2ddcd0a9f
nguytvananh <anh.NTV200035@sis.hust.edu.vn>
2024-11-30 23:21:58 +0700

    update commit

--- app/src/main/java/com/nhom10/smartmoney1/budget_categories/ui/BudgetCategoryListAdapter.java
+++ app/src/main/java/com/nhom10/smartmoney1/budget_categories/ui/BudgetCategoryListAdapter.java
@@ -75,7 +75,7 @@ public class BudgetCategoryListAdapter extends BaseAdapter
                 try{
                     Intent intent = new Intent( context , ListingCategoryTransactionsActivity.class);
                     intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
-                    intent.putExtra("danh mục", budgetCategory.getName());
+                    intent.putExtra("category", budgetCategory.getName());
                     context.startActivity(intent);
                 }
                 catch (Exception e)
